a9d0354beaec408468163fc3146a23dea1e893b5
Lizzy <qschukkink@gmail.com>
2021-02-12 13:07:32 +0100

    Update AssemblyInfo.cs

--- MQTChroma/Properties/AssemblyInfo.cs
+++ MQTChroma/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MQTChroma")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Control your Razer Chroma lights with MQTT messages")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DeadlyKltten")]
 [assembly: AssemblyProduct("MQTChroma")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
